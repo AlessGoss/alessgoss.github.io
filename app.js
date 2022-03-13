8c6eaf62f47c8e80a27fa2baa01753d15dbb72bc
Aless <54534326+AlessGoss@users.noreply.github.com>
2022-03-13 18:13:52 +0100

    HUGE IMPROVEMENT: Added light-dark mode system.

---
+++ app.js
@@ -0,0 +1,58 @@
+var buttons;
+var containers;
+var topnav;
+window.onload = function () {
+  buttons = document.getElementsByClassName("a-button");
+  containers = document.getElementsByClassName("container");
+  topnav = document.getElementsByClassName("topnav");
+};
+var lightModeSet;
+function lightMode(bool) {
+  var light = bool;
+  if (light) {
+    var l = buttons.length;
+    for (i = 0; i < l; i++) {
+      buttons[i].classList.add("a-button-light");
+    }
+
+    var l = containers.length;
+    for (i = 0; i < l; i++) {
+      containers[i].classList.add("container-light");
+    }
+
+    var l = topnav.length;
+    for (i = 0; i < l; i++) {
+      topnav[i].classList.add("topnav-light");
+    }
+
+    document.body.style.background = "#eee";
+    document.body.style.color = "#000";
+    lightModeSet = true;
+    document.getElementById("toggleThemeBtn").innerHTML =
+      '<i class="fas fa-moon"></i> (Dark mode)';
+    document
+      .getElementById("toggleThemeBtn")
+      .setAttribute("onclick", "lightMode(false)");
+  } else {
+    var l = buttons.length;
+    for (i = 0; i < l; i++) {
+      buttons[i].classList.remove("a-button-light");
+    }
+
+    var l = containers.length;
+    for (i = 0; i < l; i++) {
+      containers[i].classList.remove("container-light");
+    }
+
+    var l = topnav.length;
+    for (i = 0; i < l; i++) {
+      topnav[i].classList.remove("topnav-light");
+    }
+
+    document.body.style.background = "#111";
+    document.body.style.color = "#fff";
+    lightModeSet = false;
+    toggleThemeBtn.innerHTML = '<i class="fas fa-sun"></i> (Light mode)';
+    toggleThemeBtn.setAttribute("onclick", "lightMode(true)");
+  }
+}
