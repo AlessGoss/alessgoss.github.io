0eaab6b0d0ebed47ce3b7189d8104e619647342b
Aless <alessgoss.ctc@gmail.com>
2023-01-15 22:29:55 +0100

    Remove light-dark mode system
    I don't wanna focus on that for now.

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-var buttons;
-var containers;
-var topnav;
 var originalDocumentTitle;
 window.onload = function () {
-  buttons = document.getElementsByClassName("button");
-  containers = document.getElementsByClassName("container");
-  topnav = document.getElementsByClassName("topnav");
   overlay = document.getElementsByClassName("overlay");
 
   if (
@@ -16,66 +10,6 @@ window.onload = function () {
     document.title = "[LOCAL] " + document.title;
   }
 };
-var lightModeSet;
-function lightMode(bool) {
-  var light = bool;
-  if (light) {
-    var l = buttons.length;
-    for (i = 0; i < l; i++) {
-      buttons[i].classList.add("button-light");
-    }
-
-    var l = containers.length;
-    for (i = 0; i < l; i++) {
-      containers[i].classList.add("container-light");
-    }
-
-    var l = topnav.length;
-    for (i = 0; i < l; i++) {
-      topnav[i].classList.add("topnav-light");
-    }
-
-    document.body.style.background = "#eee";
-    document.body.style.color = "#000";
-    lightModeSet = true;
-    document.getElementById("toggleThemeBtn").innerHTML =
-      '<i class="fas fa-moon"></i>Dark mode';
-    document
-      .getElementById("toggleThemeBtn")
-      .setAttribute("onclick", "lightMode(false)");
-
-    var l = overlay.length;
-    for (i = 0; i < l; i++) {
-      overlay[i].classList.add("overlay--light-mode");
-    }
-  } else {
-    var l = buttons.length;
-    for (i = 0; i < l; i++) {
-      buttons[i].classList.remove("button-light");
-    }
-
-    var l = containers.length;
-    for (i = 0; i < l; i++) {
-      containers[i].classList.remove("container-light");
-    }
-
-    var l = topnav.length;
-    for (i = 0; i < l; i++) {
-      topnav[i].classList.remove("topnav-light");
-    }
-
-    document.body.style.background = "#111";
-    document.body.style.color = "#fff";
-    lightModeSet = false;
-    toggleThemeBtn.innerHTML = '<i class="fas fa-sun"></i>Light mode';
-    toggleThemeBtn.setAttribute("onclick", "lightMode(true)");
-
-    var l = overlay.length;
-    for (i = 0; i < l; i++) {
-      overlay[i].classList.remove("overlay--light-mode");
-    }
-  }
-}
 
 function showOverlay(overlayId, showOverlay) {
   var overlay = document.getElementById(overlayId);
