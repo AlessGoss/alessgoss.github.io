9edc116178e689e5237943d06868662749fee630
Aless <alessgoss.ctc@gmail.com>
2023-01-29 14:42:50 +0100

    Remove JavaScript completely
    That also includes the code to prepend "[LOCAL] " to the title of the website home page if it's hosted on `localhost` or `127.0.0.1`.
    
    Now my website fully works without JavaScript (for now.) I also removed the warning that shows up when JavaScript is missing, since it's not needed at the moment.

--- script.js
+++
@@ -1,7 +0,0 @@
-if (
-  window.location.hostname == "localhost" ||
-  window.location.hostname == "127.0.0.1"
-) {
-  var originalDocumentTitle = document.title;
-  document.title = "[LOCAL] " + document.title;
-}
